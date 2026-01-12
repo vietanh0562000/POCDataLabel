c102d5caccdfe8834a388de7c7e6c87083943245
vietanh0562000 <vietanh0562000@users.noreply.github.com>
2026-01-12 16:36:16 +0700

    fix

--- src/main/java/com/poc/data_assessment/model/SupportPoint.java
+++ src/main/java/com/poc/data_assessment/model/SupportPoint.java
@@ -6,17 +6,26 @@ import com.poc.data_assessment.enums.SupportPointStatus;
 
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.JoinColumn;
-import jakarta.persistence.Entity;  
+import jakarta.persistence.Entity;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.EnumType;
 import jakarta.persistence.Table;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 @Entity
 @Table(name = "SupportPoint")
 public class SupportPoint {
-    public String id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Long id;
     private Double qLkw;
     private Double qKfz;
     private Double vLkw;
     private Double vKfz;
+
+    @Enumerated(EnumType.STRING)
     private SupportPointStatus status;
     private Instant createdAt;
 
